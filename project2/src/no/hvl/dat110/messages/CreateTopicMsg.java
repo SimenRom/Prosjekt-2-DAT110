78567d01650842b1a3efb03b243c743f508acc0a
arisa <arisa@arisas-mbp>
2019-02-25 15:42:43 +0100

    CreateTopicMsg.java

--- project2/src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ project2/src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -5,4 +5,24 @@ public class CreateTopicMsg extends Message {
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
 	
+	String topic;
+	
+	public CreateTopicMsg (String topic, String user) {
+		super(MessageType.CREATETOPIC, user);
+		this.topic = topic;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+	
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "CreateTopicMsg [topic=" + topic + "]";
+	}
+	
 }
