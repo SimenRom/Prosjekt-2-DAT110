497b3a0556a70f957f892ff9914e47c9dd8e8402
simen <simen@LAPTOP-FMB2HVN5.sognenett.net>
2019-02-25 15:53:22 +0100

    PublishMsg implementert.

--- project2/src/no/hvl/dat110/messages/PublishMsg.java
+++ project2/src/no/hvl/dat110/messages/PublishMsg.java
@@ -5,4 +5,21 @@ public class PublishMsg extends Message {
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
 	
+	String message;
+	public PublishMsg(String message, String usr) {
+		super(MessageType.PUBLISH, usr);
+		this.message = message;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public String toString() {
+		return "PublishMsg [message=" + message + "]";
+	}
+	
+	
 }
