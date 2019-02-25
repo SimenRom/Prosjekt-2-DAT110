a85c894326d8f05902e4b245d92e60de6e64a64f
simen <simen@LAPTOP-FMB2HVN5.sognenett.net>
2019-02-25 15:48:23 +0100

    DeleteTopicMsg implementert.

--- project2/src/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ project2/src/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -4,5 +4,28 @@ public class DeleteTopicMsg extends Message {
 	
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
+	String topic;
+	
+	public DeleteTopicMsg(String topic, String usr) {
+		super(MessageType.DELETETOPIC, usr);
+		this.topic = topic;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "DeleteTopicMsg [topic=" + topic + "]";
+	}
+	
+	
+	
+	
 	
 }
