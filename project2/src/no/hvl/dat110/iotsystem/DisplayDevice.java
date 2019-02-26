b88f4700d3ff2ceeab0094df015c254d1769b1f3
arisa <arisa@arisas-mbp>
2019-02-26 13:07:26 +0100

    TemperatureDevice and DisplayDevice fixed. Tests ran. Still issue with Push

--- project2/src/no/hvl/dat110/iotsystem/DisplayDevice.java
+++ project2/src/no/hvl/dat110/iotsystem/DisplayDevice.java
@@ -5,20 +5,25 @@ import no.hvl.dat110.messages.Message;
 import no.hvl.dat110.messages.PublishMsg;
 
 public class DisplayDevice {
-	
+
 	private static final int COUNT = 10;
-		
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
+
+		Client c = new Client("DisplayDevice", Common.BROKERHOST, Common.BROKERPORT);
+		c.connect();
+		c.createTopic(Common.TEMPTOPIC);
+		c.subscribe(Common.TEMPTOPIC);
 		
 		System.out.println("Display starting ...");
-		
-		// TODO - START
-				
-		// TODO - END
+
+		for (int i = 0; i < COUNT; i++) {
+			PublishMsg melding = (PublishMsg) c.receive();
+			System.out.println("Temperaturen er " + melding.getMessage());
+		}
+		c.disconnect();
 		
 		System.out.println("Display stopping ... ");
-		
-		throw new RuntimeException("not yet implemented");
-		
+
 	}
 }
